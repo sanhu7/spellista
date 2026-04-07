bd4bada7a84b9597ae4f2a548947890254d0ef45
Sana Naqeeb <sansoonm2012@gmail.com>
2026-04-08 01:49:01 +0200

    HTML-skelett

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// features: Song search och filtrering 
+// @ts-nocheck
+'use strict';
+
+playlists = this.loadFromStorage();
+
+saveToStorage() {
+    localStorage.setItem('playlists', JSON.stringify(this.playlists));
+}
+
+loadFromStorage() {
+    const saved = localStorage.getItem('playlists');
+    return saved ? JSON.parse(saved) : [];
+}
+
+this.playlists.push(playlist);
+this.saveToStorage();
+
+this.playlists = this.playlists.filter(p => p.id !== id);
+this.saveToStorage();
+
+
+playlist.songs.push(song);
+this.saveToStorage();
+
+
+playlist.songs = playlist.songs.filter(s => s.id !== songId);
+this.saveToStorage(); 
